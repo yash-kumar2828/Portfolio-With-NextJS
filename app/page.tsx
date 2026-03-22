dab03fcb1664ec0ad117abcea35c1f64f69aaff1
Yash Kumar <yk5708428@gmail.com>
2026-03-22 21:39:46 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/Header'
+import About from '@/components/About'
+import Skills from '@/components/Skills'
+import Project from '@/components/Project'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+   <>
+   
+   <Header />
+   <About />
+    <Skills />
+    <Project />
+    <Contact />
+    <Footer />
+   </>
+  );
+}
